b22c014a57bcdd7d178a451af26750fe8e969478
Phil Smith <psmith@ntdev.microsoft.com>
2017-09-21 18:07:40 -0700

    Start roughing out LocalFileHeader in ZipStream

--- src/inc/Exceptions.hpp
+++ src/inc/Exceptions.hpp
@@ -6,32 +6,39 @@ namespace xPlat {
     class ExceptionBase : public std::exception
     {
     public:
-        enum Facility : public uint32_t {
+        enum Facility : std::uint32_t {
             NONE = 0x0000,
             FILE = 0x1000,
-            CRC  = 0x2000,
+            ZIP =  0x2000,
+            CRC =  0x4000,
             MAX =  0x0FFF      // always last, always defines bytes reserved for forwarding errors
-        } facility = Facility::NONE;
+        };
 
-        void SetLastError(uint32_t error)
+        ExceptionBase() {}
+        ExceptionBase(Facility facility) : facility(facility) {}
+
+        void SetLastError(std::uint32_t error)
         {
-            code = Header + facility + (error & Facility::MAX);
+            code = header + facility + (error & Facility::MAX);
         }
 
         uint32_t Code() { return code; }
 
     protected:
-        uint32_t Header = 0x8BAD0000;   // facility 2989
-        uint32_t code   = 0xFFFFFFFF;   // by default, something very bad happened...
+        Facility facility = Facility::NONE;
+        std::uint32_t header = 0x8BAD0000;   // facility 2989
+        std::uint32_t code   = 0xFFFFFFFF;   // by default, something very bad happened...
     };
 
-    class NotImplementedException : public ExceptionBase { public: NotImplementedException { SetLastError(1); } };
-    class NotSupportedException : public ExceptionBase {   public: NotSupportedException   { SetLastError(2); } };
+    class NotImplementedException : public ExceptionBase { public: NotImplementedException() { SetLastError(1); } };
+    class NotSupportedException : public ExceptionBase {   public: NotSupportedException()   { SetLastError(2); } };
 
     class FileException : public ExceptionBase
     {
     public:
-        FileException(std::string& message, int error = 0) : reason(message), facility(Facility::FILE)
+        FileException(std::string message, std::uint32_t error = 0) :
+            reason(message),
+            ExceptionBase(ExceptionBase::Facility::FILE)
         {
             SetLastError(error);
         }
