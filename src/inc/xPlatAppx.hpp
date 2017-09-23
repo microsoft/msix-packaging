156b2e0881f80281b0f88d0c907b23108617a867
Phil Smith <msft_phil@hotmail.com>
2017-09-22 21:32:32 -0700

    Create initial xcode project and fix a few cross platform issues.

--- src/inc/xPlatAppx.hpp
+++ src/inc/xPlatAppx.hpp
@@ -1,10 +1,15 @@
 #pragma once
 
-#ifdef XPLATAPPX_API
-#define XPLATAPPX_API __declspec(dllexport)
-#else  
-#define XPLATAPPX_API __declspec(dllimport)
-#endif 
+#ifdef PLATFORM_APPLE
+    #define XPLATAPPX_API
+
+#else /*PLATFORM_MAC*/
+    #ifdef XPLATAPPX_API
+        #define XPLATAPPX_API __declspec(dllexport)
+    #else /*XPLATAPPX_API*/
+        #define XPLATAPPX_API __declspec(dllimport)
+    #endif /*XPLATAPPX_API*/
+#endif /*PLATFORM_MAC*/
 
 // TODO: add #defs to cover platform-specific differences?
 XPLATAPPX_API unsigned int UnpackAppx (char* source, char* destination);
