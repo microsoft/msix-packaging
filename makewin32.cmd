5c05e3310ca1551fae03215bfeaca6da470e2220
Phil Smith <psmith@microsoft.com>
2017-10-27 18:48:31 +0000

    Merged PR 1028022: Merge xerces to feature
    The latest round of ES changes that I had to make to get NuGet package publishing working.  You can see what the release logs look like here: https://microsoft.visualstudio.com/xPlatAppx/_apps/hub/ms.vss-releaseManagement-web.hub-explorer?definitionId=2&releaseId=17&_a=release-logs and you can find the published package here: https://microsoft.visualstudio.com/xPlatAppx/_packaging?feed=xPlatAppx&_a=feed .

--- makewin32.cmd
+++ makewin32.cmd
@@ -1,6 +1,8 @@
 REM HUZZA FOR: https://dmerej.info/blog/post/cmake-visual-studio-and-the-command-line/
-md .vs
+if not exist .vs md .vs
 cd .vs
+if exist CMakeFiles rd /s /q CMakeFiles
+if exist CMakeCache.txt del CMakeCache.txt
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\Tools\VsDevCmd.bat"
 cmake -DWIN32=on -G"NMake Makefiles" ..
 nmake
