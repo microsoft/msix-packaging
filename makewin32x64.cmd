b02e1ff6b5f64f9990ffda740b5a16cb8b7f3e89
Phil Smith <psmith@microsoft.com>
2017-12-04 22:35:57 +0000

    Merged PR 1179009: Merge bug_14942409 to feature
    Enable 64-bit builds for Windows platforms.

---
+++ makewin32x64.cmd
@@ -0,0 +1,8 @@
+REM HUZZA FOR: https://dmerej.info/blog/post/cmake-visual-studio-and-the-command-line/
+if not exist .vs md .vs
+cd .vs
+if exist CMakeFiles rd /s /q CMakeFiles
+if exist CMakeCache.txt del CMakeCache.txt
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
+cmake -DWIN32=on -DCMAKE_BUILD_TYPE=Release -G"NMake Makefiles" ..
+nmake
