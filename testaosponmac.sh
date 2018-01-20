7cac2c9a588d8b0516e5d470455983f6c365d79f
Ruben Guerrero <rubengu@microsoft.com>
2018-01-19 17:33:00 -0800

    stabilization

--- testaosponmac.sh
+++ testaosponmac.sh
@@ -2,7 +2,7 @@
 testfailed=0
 xplatappxdir=`pwd`
 
-function RunCommand {
+function RunCommandWithTimeout {
 	local result=1
 	local waitingtime=0
 	while [ $result -ne 0 ]
@@ -20,11 +20,23 @@ function RunCommand {
 	done
 }
 
+function RunCommand {
+	$1
+	local result=$?
+	if [ $result -ne 0 ]
+	then
+		echo "Setup failure"
+		exit 1
+	fi
+}
+
 function StartEmulator {
 	cd $ANDROID_HOME/tools
 	emulator -avd Nexus_5X_API_19_x86 -netdelay none -netspeed full &
-	RunCommand "adb shell getprop dev.bootcomplete"
-	RunCommand "adb shell getprop init.svc.bootanim"
+	RunCommandWithTimeout "adb shell getprop dev.bootcomplete"
+	RunCommandWithTimeout "adb shell getprop init.svc.bootanim"
+	# At this time the device booted, but give some time to stabilize
+	sleep 30 
 	echo "Android emulator started"
 }
 
@@ -41,14 +53,14 @@ function CreateApp {
 
 function RunTest {
 	# Install app
-	adb push app/build/outputs/apk/debug/app-debug.apk /data/local/tmp/com.microsoft.xplatappxandroid
-	adb shell pm install -t -r "/data/local/tmp/com.microsoft.xplatappxandroid"
+	RunCommand "adb push app/build/outputs/apk/debug/app-debug.apk /data/local/tmp/com.microsoft.xplatappxandroid"
+	RunCommand "adb shell pm install -t -r '/data/local/tmp/com.microsoft.xplatappxandroid'"
 	# Start app
-	adb shell am start -n "com.microsoft.xplatappxandroid/com.microsoft.xplatappxandroid.MainActivity" -a android.intent.action.MAIN -c android.intent.category.LAUNCHER
+	RunCommand "adb shell am start -n 'com.microsoft.xplatappxandroid/com.microsoft.xplatappxandroid.MainActivity' -a android.intent.action.MAIN -c android.intent.category.LAUNCHER"
 	# The apps terminates when is done
 	sleep 5
 	# Get Results
-	adb pull /data/data/com.microsoft.xplatappxandroid/files/testResults.txt
+	RunCommand "adb pull /data/data/com.microsoft.xplatappxandroid/files/testResults.txt"
 }
 
 function ParseResult {
@@ -69,7 +81,7 @@ function ParseResult {
 }
 
 StartEmulator
-# Clean up
+# Clean up. This commands might fail, but is not an error
 adb shell rm -r /data/data/com.microsoft.xplatappxandroid/files
 rm $xplatappxdir/test/mobile/xPlatAppxAndroid/testResults.txt
 
