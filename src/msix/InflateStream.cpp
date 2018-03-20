00e628fccb92958c86880ab098404046bacc33c7
Phil Smith <psmith@ntdev.microsoft.com>
2018-03-19 17:47:36 -0700

    Remove std::function and std::map from InflateStream. Change saves a few more KB.

--- src/msix/InflateStream.cpp
+++ src/msix/InflateStream.cpp
@@ -11,8 +11,121 @@
 #include <cassert>
 #include <algorithm>
 #include <cstring>
+#include <array>
+#include <utility>
 
 namespace MSIX {
+    struct InflateHandler
+    {
+        typedef std::pair<bool, InflateStream::State>(*lambda)(InflateStream* self, void* buffer, ULONG countBytes);
+        InflateHandler(lambda f): Handler(f) {}
+        lambda Handler;
+    };
+
+    std::array<InflateHandler, InflateStream::State::MAX> stateMachine =
+    {            
+        // State::UNINITIALIZED
+        InflateHandler([](InflateStream* self, void*, ULONG)
+        {
+            ThrowHrIfFailed(self->m_stream->Seek({0}, StreamBase::START, nullptr));
+            self->m_zstrm = { 0 };
+            self->m_fileCurrentPosition = 0;
+            self->m_fileCurrentWindowPositionEnd = 0;
+
+            int ret = inflateInit2(&(self->m_zstrm), -MAX_WBITS);
+            ThrowErrorIfNot(Error::InflateInitialize, (ret == Z_OK), "inflateInit2 failed");
+            return std::make_pair(true, InflateStream::State::READY_TO_READ);
+        }), // State::UNINITIALIZED
+
+        // State::READY_TO_READ
+        InflateHandler([](InflateStream* self, void*, ULONG)
+        {
+            ThrowErrorIfNot(Error::InflateRead,(self->m_zstrm.avail_in == 0), "uninflated bytes overwritten");
+            ULONG available = 0;
+            ThrowHrIfFailed(self->m_stream->Read(self->m_compressedBuffer, InflateStream::BUFFERSIZE, &available));
+            ThrowErrorIf(Error::FileRead, (available == 0), "Getting nothing back is unexpected here.");
+            self->m_zstrm.avail_in = static_cast<uInt>(available);
+            self->m_zstrm.next_in = self->m_compressedBuffer;
+            return std::make_pair(true, InflateStream::State::READY_TO_INFLATE);
+        }), // State::READY_TO_READ
+
+        // State::READY_TO_INFLATE
+        InflateHandler([](InflateStream* self, void*, ULONG)
+        {
+            self->m_inflateWindowPosition = 0;
+            self->m_zstrm.avail_out = InflateStream::BUFFERSIZE;
+            self->m_zstrm.next_out = self->m_inflateWindow;
+            self->m_zret = inflate(&(self->m_zstrm), Z_NO_FLUSH);
+            switch (self->m_zret)
+            {
+            case Z_NEED_DICT:
+            case Z_DATA_ERROR:
+            case Z_MEM_ERROR:
+                self->Cleanup();
+                ThrowErrorIfNot(Error::InflateCorruptData, false, "inflate failed unexpectedly.");
+            case Z_STREAM_END:
+            default:
+                self->m_fileCurrentWindowPositionEnd += (InflateStream::BUFFERSIZE - self->m_zstrm.avail_out);
+                return std::make_pair(true, InflateStream::State::READY_TO_COPY);
+            }
+        }), // State::READY_TO_INFLATE
+
+        // State::READY_TO_COPY
+        InflateHandler([](InflateStream* self, void* buffer, ULONG countBytes)
+        {
+            // Check if we're actually at the end of stream.
+            if (self->m_fileCurrentPosition >= self->m_uncompressedSize)
+            {
+                ThrowErrorIfNot(Error::InflateCorruptData, ((self->m_zret == Z_STREAM_END) && (self->m_zstrm.avail_in == 0)), "unexpected extra data");
+                return std::make_pair(true, InflateStream::State::CLEANUP);
+            }
+
+            // If the end of the current window position is less than the seek position, keep inflating
+            if (self->m_fileCurrentWindowPositionEnd < self->m_seekPosition)
+            {
+                self->m_fileCurrentPosition += self->m_zstrm.avail_out;
+                return std::make_pair(true, (self->m_zstrm.avail_in == 0) ? InflateStream::State::READY_TO_READ : InflateStream::State::READY_TO_INFLATE);
+            }
+
+            // now that we're within the window between current file position and seek position
+            // calculate the number of bytes to skip ahead within this window
+            ULONG bytesToSkipInWindow = (ULONG)(self->m_seekPosition - self->m_fileCurrentPosition);
+            self->m_inflateWindowPosition += bytesToSkipInWindow;
+
+            // Calculate the difference between the beginning of the window and the seek position.
+            // if there's nothing left in the window to copy, then we need to fetch another window.
+            ULONG bytesRemainingInWindow = (InflateStream::BUFFERSIZE - self->m_zstrm.avail_out) - self->m_inflateWindowPosition;
+            if (bytesRemainingInWindow == 0)
+            {
+                return std::make_pair(true, (self->m_zstrm.avail_in == 0) ? InflateStream::State::READY_TO_READ : InflateStream::State::READY_TO_INFLATE);
+            }
+
+            ULONG bytesToCopy = std::min(countBytes, bytesRemainingInWindow);
+            if (bytesToCopy > 0)
+            {
+                memcpy(buffer, &(self->m_inflateWindow[self->m_inflateWindowPosition]), bytesToCopy);
+                self->m_bytesRead             += bytesToCopy;
+                self->m_seekPosition          += bytesToCopy;
+                self->m_inflateWindowPosition += bytesToCopy;
+                self->m_fileCurrentPosition   += bytesToCopy;
+            }
+
+            if (self->m_fileCurrentPosition == self->m_uncompressedSize)
+            {
+                self->Cleanup();
+                return std::make_pair(false, InflateStream::State::UNINITIALIZED);
+            }
+            return std::make_pair(countBytes != 0, InflateStream::State::READY_TO_COPY);
+        }), // State::READY_TO_COPY
+
+        // State::CLEANUP    
+        InflateHandler([](InflateStream* self, void*, ULONG)
+        {
+            self->Cleanup();
+            return std::make_pair(false, InflateStream::State::UNINITIALIZED);
+        }) // State::CLEANUP            
+    };
+
     InflateStream::InflateStream(
         IStream* stream, std::uint64_t uncompressedSize
     ) : m_stream(stream),
@@ -20,105 +133,6 @@ namespace MSIX {
         m_uncompressedSize(uncompressedSize)
     {
         m_zstrm = {0};
-        m_stateMachine =
-        {
-            { State::CLEANUP, [&](void*, ULONG )
-                {
-                    Cleanup();
-                    return std::make_pair(false, State::UNINITIALIZED);
-                }
-            }, // State::CLEANUP
-            { State::UNINITIALIZED , [&](void*, ULONG)
-                {
-                    ThrowHrIfFailed(m_stream->Seek({0}, StreamBase::START, nullptr));
-                    m_zstrm = { 0 };
-                    m_fileCurrentPosition = 0;
-                    m_fileCurrentWindowPositionEnd = 0;
-
-                    int ret = inflateInit2(&m_zstrm, -MAX_WBITS);
-                    ThrowErrorIfNot(Error::InflateInitialize, (ret == Z_OK), "inflateInit2 failed");
-                    return std::make_pair(true, State::READY_TO_READ);
-                }
-            }, // State::UNINITIALIZED
-            { State::READY_TO_READ , [&](void*, ULONG)
-                {
-                    ThrowErrorIfNot(Error::InflateRead,(m_zstrm.avail_in == 0), "uninflated bytes overwritten");
-                    ULONG available = 0;
-                    ThrowHrIfFailed(m_stream->Read(m_compressedBuffer, InflateStream::BUFFERSIZE, &available));
-                    ThrowErrorIf(Error::FileRead, (available == 0), "Getting nothing back is unexpected here.");
-                    m_zstrm.avail_in = static_cast<uInt>(available);
-                    m_zstrm.next_in = m_compressedBuffer;
-                    return std::make_pair(true, State::READY_TO_INFLATE);
-                }
-            }, // State::READY_TO_READ
-            { State::READY_TO_INFLATE, [&](void*, ULONG)
-                {
-                    m_inflateWindowPosition = 0;
-                    m_zstrm.avail_out = InflateStream::BUFFERSIZE;
-                    m_zstrm.next_out = m_inflateWindow;
-                    m_zret = inflate(&m_zstrm, Z_NO_FLUSH);
-                    switch (m_zret)
-                    {
-                    case Z_NEED_DICT:
-                    case Z_DATA_ERROR:
-                    case Z_MEM_ERROR:
-                        Cleanup();
-                        ThrowErrorIfNot(Error::InflateCorruptData, false, "inflate failed unexpectedly.");
-                    case Z_STREAM_END:
-                    default:
-                        m_fileCurrentWindowPositionEnd += (InflateStream::BUFFERSIZE - m_zstrm.avail_out);
-                        return std::make_pair(true, State::READY_TO_COPY);
-                    }
-                }
-            }, // State::READY_TO_INFLATE
-            { State::READY_TO_COPY , [&](void* buffer, ULONG countBytes)
-                {
-                    // Check if we're actually at the end of stream.
-                    if (m_fileCurrentPosition >= m_uncompressedSize)
-                    {
-                        ThrowErrorIfNot(Error::InflateCorruptData, ((m_zret == Z_STREAM_END) && (m_zstrm.avail_in == 0)), "unexpected extra data");
-                        return std::make_pair(true, State::CLEANUP);
-                    }
-
-                    // If the end of the current window position is less than the seek position, keep inflating
-                    if (m_fileCurrentWindowPositionEnd < m_seekPosition)
-                    {
-                        m_fileCurrentPosition += m_zstrm.avail_out;
-                        return std::make_pair(true, (m_zstrm.avail_in == 0) ? State::READY_TO_READ : State::READY_TO_INFLATE);
-                    }
-
-                    // now that we're within the window between current file position and seek position
-                    // calculate the number of bytes to skip ahead within this window
-                    ULONG bytesToSkipInWindow = (ULONG)(m_seekPosition - m_fileCurrentPosition);
-                    m_inflateWindowPosition += bytesToSkipInWindow;
-
-                    // Calculate the difference between the beginning of the window and the seek position.
-                    // if there's nothing left in the window to copy, then we need to fetch another window.
-                    ULONG bytesRemainingInWindow = (InflateStream::BUFFERSIZE - m_zstrm.avail_out) - m_inflateWindowPosition;
-                    if (bytesRemainingInWindow == 0)
-                    {
-                        return std::make_pair(true, (m_zstrm.avail_in == 0) ? State::READY_TO_READ : State::READY_TO_INFLATE);
-                    }
-
-                    ULONG bytesToCopy = std::min(countBytes, bytesRemainingInWindow);
-                    if (bytesToCopy > 0)
-                    {
-                        memcpy(buffer, &(m_inflateWindow[m_inflateWindowPosition]), bytesToCopy);
-                        m_bytesRead             += bytesToCopy;
-                        m_seekPosition          += bytesToCopy;
-                        m_inflateWindowPosition += bytesToCopy;
-                        m_fileCurrentPosition   += bytesToCopy;
-                    }
-
-                    if (m_fileCurrentPosition == m_uncompressedSize)
-                    {
-                        Cleanup();
-                        return std::make_pair(false, State::UNINITIALIZED);
-                    }
-                    return std::make_pair(countBytes != 0, State::READY_TO_COPY);
-                }
-            } // State::READY_TO_COPY
-        };
     }
 
     InflateStream::~InflateStream()
@@ -136,8 +150,7 @@ namespace MSIX {
                 bool stayInLoop = true;
                 while (stayInLoop && (m_bytesRead < countBytes))
                 {
-                    const auto& stateHandler = m_stateMachine[m_state];
-                    auto&& result = stateHandler(m_startCurrentBuffer + m_bytesRead, countBytes - m_bytesRead);
+                    auto&& result = stateMachine[m_state].Handler(this, m_startCurrentBuffer + m_bytesRead, countBytes - m_bytesRead);
                     stayInLoop = std::get<0>(result);
                     m_previous = m_state;
                     m_state = std::get<1>(result);
