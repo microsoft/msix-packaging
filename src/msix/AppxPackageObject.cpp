bf2e8560d0578bacb392026eecd0ae6bbec1c5e8
Ruben Guerrero Samaniego <rubengu@microsoft.com>
2018-04-02 14:53:45 -0700

    Fix build break

--- src/msix/AppxPackageObject.cpp
+++ src/msix/AppxPackageObject.cpp
@@ -383,7 +383,7 @@ namespace MSIX {
     HRESULT STDMETHODCALLTYPE AppxPackageObject::GetManifest(IAppxManifestReader** manifestReader) noexcept
     {
         return static_cast<HRESULT>(Error::NotImplemented);
-    } CATCH_RETURN();
+    }
 
     // IAppxBundleReader
     HRESULT STDMETHODCALLTYPE AppxPackageObject::GetFootprintFile(APPX_BUNDLE_FOOTPRINT_FILE_TYPE fileType, IAppxFile **footprintFile) noexcept try
