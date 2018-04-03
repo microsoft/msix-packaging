cd79df083854e35822bef433615211bdd462d4d5
Ruben Guerrero Samaniego <rubengu@microsoft.com>
2018-04-03 14:54:41 -0700

    Skip signature for non-Win tests

--- test/MacOS-Linux/MacOS-Linux-Etc.sh
+++ test/MacOS-Linux/MacOS-Linux-Etc.sh
@@ -81,10 +81,10 @@ RunTest 0  ./../appx/sdx/en-us_win32.appx
 # Bundle tests
 RunTest 81 ./../appx/bundles/BlockMapContainsPayloadPackage.appxbundle -ss
 RunTest 51 ./../appx/bundles/BlockMapIsMissing.appxbundle -ss
-RunTest 2 ./../appx/bundles/BlockMapViolatesSchema.appxbundle
+RunTest 2 ./../appx/bundles/BlockMapViolatesSchema.appxbundle -ss
 #RunTest 0 ./../appx/bundles/ContainsNeutralAndX86AppPackages.appxbundle
-RunTest 2 ./../appx/bundles/ContainsNoPayload.appxbundle
-RunTest 97 ./../appx/bundles/ContainsOnlyResourcePackages.appxbundle
+RunTest 2 ./../appx/bundles/ContainsNoPayload.appxbundle -ss
+RunTest 97 ./../appx/bundles/ContainsOnlyResourcePackages.appxbundle -ss
 #RunTest 0 ./../appx/bundles/ContainsTwoNeutralAppPackages.appxbundle
 RunTest 0 ./../appx/bundles/MainBundle.appxbundle -ss
 #RunTest 0 ./../appx/bundles/ManifestDeclaresAppPackageForResourcePackage.appxbundle
@@ -98,11 +98,11 @@ RunTest 52 ./../appx/bundles/ManifestIsMissing.appxbundle -ss
 #RunTest 0 ./../appx/bundles/ManifestPackageHasIncorrectVersion.appxbundle
 #RunTest 0 ./../appx/bundles/ManifestPackageHasInvalidOffset.appxbundle
 #RunTest 0 ./../appx/bundles/ManifestPackageHasInvalidRange.appxbundle
-RunTest 2 ./../appx/bundles/ManifestViolatesSchema.appxbundle
-RunTest 97 ./../appx/bundles/PayloadPackageHasNonAppxExtension.appxbundle
-RunTest 97 ./../appx/bundles/PayloadPackageIsCompressed.appxbundle
-RunTest 3 ./../appx/bundles/PayloadPackageIsEmpty.appxbundle.zip
-RunTest 87 ./../appx/bundles/PayloadPackageIsNotAppxPackage.appxbundle
+RunTest 2 ./../appx/bundles/ManifestViolatesSchema.appxbundle -ss
+RunTest 97 ./../appx/bundles/PayloadPackageHasNonAppxExtension.appxbundle -ss
+RunTest 97 ./../appx/bundles/PayloadPackageIsCompressed.appxbundle -ss
+RunTest 3 ./../appx/bundles/PayloadPackageIsEmpty.appxbundle -ss
+RunTest 87 ./../appx/bundles/PayloadPackageIsNotAppxPackage.appxbundle -ss
 #RunTest 0 ./../appx/bundles/PayloadPackageNotListedInManifest.appxbundle
 RunTest 66 ./../appx/bundles/SignedUntrustedCert-CERT_E_CHAINING.appxbundle
 RunTest 0 ./../appx/bundles/StoreSigned_Desktop_x86_x64_MoviesTV.appxbundle
