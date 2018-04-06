e0747addcd994a2be5318143fcc6baa5b070fde4
Ruben Guerrero Samaniego <rubengu@microsoft.com>
2018-04-06 00:47:53 +0000

    Enable a switch to make zlib a dynamic dependency for msix.dll

--- makewin32chk.cmd
+++ makewin32chk.cmd
@@ -3,6 +3,10 @@ if not exist .vs md .vs
 cd .vs
 if exist CMakeFiles rd /s /q CMakeFiles
 if exist CMakeCache.txt del CMakeCache.txt
+echo %cd%
+set currentpath=%cd%
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\Tools\VsDevCmd.bat"
-cmake -DWIN32=on -DCMAKE_BUILD_TYPE=Debug %1 -G"NMake Makefiles" ..
+cd /D %currentpath%
+echo cmake -DWIN32=on -DCMAKE_BUILD_TYPE=MinSizeRel %1 %2 -G"NMake Makefiles" ..
+cmake -DWIN32=on -DCMAKE_BUILD_TYPE=Debug %1 %2 -G"NMake Makefiles" ..
 nmake
