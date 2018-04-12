78542ce3a593c42b8dfbaf74979fcb7fde9b41a0
Ruben Guerrero Samaniego <rubengu@microsoft.com>
2018-04-12 10:38:22 -0700

    Finish Win7 PAL and start of Linux and Apple PAL

---
+++ src/msix/PAL/Applicability/Apple/Applicability.cpp
@@ -0,0 +1,41 @@
+//
+//  Copyright (C) 2017 Microsoft.  All rights reserved.
+//  See LICENSE file in the project root for full license information.
+//
+
+#include <CoreFoundation/CoreFoundation.h>
+#include <set>
+
+namespace MSIX {
+
+    MSIX_PLATFORM Applicability::GetPlatform()
+    {
+    #ifdef IOS
+        return MSIX_PLATFORM_IOS;
+    #elif MACOS
+        return MSIX_PLATFORM_MACOS;
+    #else
+        UNEXPECTED;
+    #endif
+    }
+
+    std::set<std::string> Applicability::GetLanguages()
+    {
+        std::set<std::string> result;
+        CFArrayRef preferredLanguages = CFLocaleCopyPreferredLanguages();
+        CFIndex size = CFArrayGetCount(preferredLanguages);
+        for(int i = 0; i < size; i++)
+        {
+            CFStringRef value = static_cast<CFStringRef>(CFArrayGetValueAtIndex(preferredLanguages, i));
+            CFIndex valueSize = CFStringGetLength(value) + 1; // +1 for null termination
+            char buffer[valueSize];
+            if(CFStringGetCString(value, buffer, valueSize, kCFStringEncodingUTF8))
+            {
+                // TODO: verify if this API returns zh-Hans-CN or zh-CN
+                languages.insert(std::string(buffer));
+            }
+        }
+        CFRelease(preferredLanguages);
+        return result;
+    }
+}
