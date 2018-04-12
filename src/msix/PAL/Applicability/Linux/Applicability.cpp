d45cfc34b3addf810f521b676b85da4eb8eea02e
Ruben Guerrero Samaniego <rubengu@microsoft.com>
2018-04-12 10:58:48 -0700

    Linux PAL without special cases

--- src/msix/PAL/Applicability/Linux/Applicability.cpp
+++ src/msix/PAL/Applicability/Linux/Applicability.cpp
@@ -7,6 +7,10 @@
 #include <unicode/utypes.h>
 #include <set>
 
+#include "AppxPackaging.hpp"
+#include "Applicability.hpp"
+#include "Exceptions.hpp"
+
 namespace MSIX {
 
     MSIX_PLATFORM Applicability::GetPlatform() { return MSIX_PLATFORM_LINUX; }
@@ -17,12 +21,9 @@ namespace MSIX {
         UErrorCode status = U_ZERO_ERROR;
         char bcp47[ULOC_FULLNAME_CAPACITY];
         int bcp47Lenght = uloc_toLanguageTag(uloc_getDefault(), bcp47, ULOC_FULLNAME_CAPACITY, true, &status);
-        if(U_FAILURE(status) || status == U_STRING_NOT_TERMINATED_WARNING)
-        {
-            //throw exception
-        }
+        ThrowErrorIf(Error::Unexpected, U_FAILURE(status) || status == U_STRING_NOT_TERMINATED_WARNING, "Failure getting the system language");
         // TODO: verify if this API returns zh-Hans-CN or zh-CN
-        results.insert(std::string(bcp47));
+        result.insert(bcp47);
         return result;
     }
 }
