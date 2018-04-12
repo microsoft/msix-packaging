438b02477aa26976c5df211a09bef8fc0015a2f4
Ruben Guerrero Samaniego <rubengu@microsoft.com>
2018-04-12 10:38:22 -0700

    Finish Win7 PAL and start of Linux and Apple PAL

---
+++ src/msix/PAL/Applicability/Linux/Applicability.cpp
@@ -0,0 +1,28 @@
+//
+//  Copyright (C) 2017 Microsoft.  All rights reserved.
+//  See LICENSE file in the project root for full license information.
+//
+
+#include <unicode/uloc.h>
+#include <unicode/utypes.h>
+#include <set>
+
+namespace MSIX {
+
+    MSIX_PLATFORM Applicability::GetPlatform() { return MSIX_PLATFORM_LINUX; }
+
+    std::set<std::string> Applicability::GetLanguages()
+    {
+        std::set<std::string> result;
+        UErrorCode status = U_ZERO_ERROR;
+        char bcp47[ULOC_FULLNAME_CAPACITY];
+        int bcp47Lenght = uloc_toLanguageTag(uloc_getDefault(), bcp47, ULOC_FULLNAME_CAPACITY, true, &status);
+        if(U_FAILURE(status) || status == U_STRING_NOT_TERMINATED_WARNING)
+        {
+            //throw exception
+        }
+        // TODO: verify if this API returns zh-Hans-CN or zh-CN
+        results.insert(std::string(bcp47));
+        return result;
+    }
+}
