0266c6316e8459725adfaa22cc3fe131f7d5169f
Ruben Guerrero Samaniego <rubengu@microsoft.com>
2018-05-23 21:38:09 +0000

    Merged PR 1862819: Support having more than one language on Android for devices running on level 24 or higher.
    Android added, on API level 24 support for having more than one language on the device . MSIX min API level is 19, so we can't use Configuration.getLocales() to obtain them and we have to use the deprecated attribute Configuration.locale. We should support returning multiple languages for devices were it is supported and only one when is not possible.
    
    Also add documentation on the README about Android support and removed expecting always $ANDROID_HOME as an environment variable by adding a new switch on CMake that expects the SDK path.
    
    There isn't a problem in building a jar with a higher API level and then run it on a lower API level device. However, there is a problem in building the native lib on a higher API level than one of the device. This is why the distinction has to be made between the NDK and SDK level. In less words, the NDK minimum is 19 and the SDK minimum is 24.
    
    Related work items: #17634105

--- makeaosp.sh
+++ makeaosp.sh
@@ -5,41 +5,53 @@ ndk=
 arch=x86
 build=MinSizeRel
 version=19
+sdk=
+sdkver=24
 
 usage()
 {
-    echo "usage: makeaosp [-ndk ndk_path] [-arch arch] [-v version] [-b buildType]"
+    echo "usage: makeaosp [-ndk ndk_path] [-arch arch] [-ndkver ndk_version] [-sdk sdk_path] [-sdkver sdk_version] [-b buildType]"
     echo $'\t' "-ndk Path to Android NDK. Default $ANDROID_NDK_ROOT or $ANDROID_NDK"
+    echo $'\t' "-ndkver Android NDK version. Default/minimum 19."
+    echo $'\t' "-sdk Path to Android SDK. Default $ANDROID_HOME."
+    echo $'\t' "-sdkver Android SDK version. Default/minimum 24."
     echo $'\t' "-arch Architecture ABI. Default x86"
-    echo $'\t' "-v Android version. Default 19"
     echo $'\t' "-b Build type. Default MinSizeRel"
 }
 
 printsetup()
 {
     echo "NDK Path:" $ndk
+    echo "NDK Version:" $version
+    echo "SDK Path:" $sdk
+    echo "SDK Version:" $sdkver
     echo "Architecture:" $arch
-    echo "Version:" $version
     echo "Build Type:" $build
 }
 
 while [ "$1" != "" ]; do
     case $1 in
-        -ndk )  shift
-                ndk=$1
-                ;;
-        -arch ) shift
-                arch=$1
-                ;;
-        -b )    shift
-                build=$1
-                ;;
-        -v )    shift
-                version=$1
-                ;;
-        -h )    usage
-                exit
-                ;;
+        -ndk )    shift
+                  ndk=$1
+                  ;;
+        -arch )   shift
+                  arch=$1
+                  ;;
+        -b )      shift
+                  build=$1
+                  ;;
+        -ndkver ) shift
+                  version=$1
+                  ;;
+        -h )      usage
+                  exit
+                  ;;
+        -sdk )    shift
+                  sdk=$1
+                  ;;
+        -sdkver ) shift
+                  sdkver=$1
+                  ;;
         * )     usage
                 exit 1
     esac
@@ -54,6 +66,14 @@ elif [ -z "$ndk" ]; then
     echo "Android NDK not found"
     exit 1
 fi
+
+if [ -z "$sdk" ] && [ -n "$ANDROID_HOME" ]; then
+    sdk="$ANDROID_HOME"
+elif [ -z "$sdk" ]; then
+    echo "Android SDK not found"
+    exit 1
+fi
+
 printsetup
 
 mkdir .vs
@@ -65,6 +85,8 @@ find . -name *msix* -d | xargs rm -r
 cmake -DCMAKE_SYSTEM_NAME=Android \
     -DCMAKE_ANDROID_NDK="$ndk" \
     -DCMAKE_SYSTEM_VERSION="$version" \
+    -DANDROID_SDK="$sdk" \
+    -DANDROID_SDK_VERSION="$sdkver" \
     -DCMAKE_ANDROID_ARCH_ABI="$arch" \
     -DCMAKE_ANDROID_NDK_TOOLCHAIN_VERSION=clang \
     -DCMAKE_ANDROID_STL_TYPE=c++_shared \
