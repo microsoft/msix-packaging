b7cc6ec13f90af489c177ab74663b569499cc416
Adrian Mascarenhas <Adrian.Mascarenhas@microsoft.com>
2018-08-17 21:46:38 +0000

    Merged PR 2234675: [MSIX SDK] Switch to use inbox implementation of zlib for ios and macos
    Zlib uses up ~25K of the msix dll size. By switching to using libcompression.dylib on MacOS and iOS we can reclaim this size back.
    LibCompression is available from iOS9.0+ and macOS 10.11+. By default we will now use libCompression apis. But if needed, we can pass in a -zlib flag to the makemac/makeios scripts to go to old behavior of using static zlib. Waiting on feedback from office folks, since office on mac is supported on macOS10.10 which does not have libcompression apis.
    
    how tested:
    -Built on windows and ran windows tests. All passed.
    -Build on mac and ran macos tests. All passed.
    -Build for ios and ran the ios test app. All passed.
    
    Related work items: #18607656

--- makemac.sh
+++ makemac.sh
@@ -1,22 +1,29 @@
 #!/bin/bash
 # script to build on mac
 build=MinSizeRel
+dataCompressionLib=libcompression
 
 usage()
 {
-    echo "usage: makemac [-b buildType]"
+    echo "usage: makemac [-b buildType] [-zlib]"
     echo $'\t' "-b Build type. Default MinSizeRel"
+    echo $'\t' "-zlib Use Zlib instead of inbox libCompression api. Default on MacOS is libCompression."
 }
 
 printsetup()
 {
     echo "Build Type:" $build
+    echo "Data Compression library:" $dataCompressionLib
 }
 
 while [ "$1" != "" ]; do
     case $1 in
         -b )    shift
                 build=$1
+                ;;
+        -zlib ) shift
+                dataCompressionLib=zlib
+                zlib="-DUSE_ZLIB=on"
                 ;;
         -h )    usage
                 exit
@@ -34,5 +41,5 @@ cd .vs
 # clean up any old builds of msix modules
 find . -name *msix* -d | xargs rm -r
 
-cmake -DCMAKE_BUILD_TYPE=$build -DMACOS=on ..
+cmake -DCMAKE_BUILD_TYPE=$build $zlib -DMACOS=on ..
 make
---
+++ src/msix/PAL/DataCompression/Zlib/InflateStream.hpp
@@ -0,0 +1,94 @@
+//
+//  Copyright (C) 2017 Microsoft.  All rights reserved.
+//  See LICENSE file in the project root for full license information.
+// 
+#pragma once
+#ifdef WIN32
+#include "zlib.h"
+#else
+#include <zlib.h>
+#endif
+
+#include "Exceptions.hpp"
+#include "StreamBase.hpp"
+#include "ComHelper.hpp"
+
+// Windows.h defines max and min... 
+#undef max
+#undef min
+#include <string>
+#include <map>
+#include <functional>
+#include <vector>
+
+namespace MSIX {
+
+    // This represents a LZW-compressed stream
+    class InflateStream final : public StreamBase
+    {
+    public:
+        InflateStream(IStream* stream, std::uint64_t uncompressedSize);
+        ~InflateStream();
+
+        HRESULT STDMETHODCALLTYPE Seek(LARGE_INTEGER move, DWORD origin, ULARGE_INTEGER *newPosition) noexcept override;
+        HRESULT STDMETHODCALLTYPE Read(void* buffer, ULONG countBytes, ULONG* bytesRead) noexcept override;
+        HRESULT STDMETHODCALLTYPE Write(void const *buffer, ULONG countBytes, ULONG *bytesWritten) noexcept override
+        {
+            return static_cast<HRESULT>(Error::NotImplemented);
+        }
+
+        HRESULT STDMETHODCALLTYPE GetSize(UINT64* size) noexcept override
+        {
+            if (size) { *size = m_uncompressedSize; }
+            return static_cast<HRESULT>(Error::OK);
+        }
+
+        HRESULT STDMETHODCALLTYPE GetCompressionOption(APPX_COMPRESSION_OPTION* compressionOption) noexcept override try
+        {   // The underlying ZipFileStream object knows, so go ask it.
+            return m_stream.As<IAppxFile>()->GetCompressionOption(compressionOption);
+        } CATCH_RETURN();
+
+        HRESULT STDMETHODCALLTYPE GetName(LPWSTR* fileName) noexcept override try
+        {   // The underlying ZipFileStream object knows, so go ask it.
+            return m_stream.As<IAppxFile>()->GetName(fileName);
+        } CATCH_RETURN();
+
+        HRESULT STDMETHODCALLTYPE GetContentType(LPWSTR* contentType) noexcept override try
+        {   // The underlying ZipFileStream object knows, so go ask it.
+            return m_stream.As<IAppxFile>()->GetContentType(contentType);
+        } CATCH_RETURN();
+
+        // IAppxFileInternal
+        std::uint64_t GetCompressedSize() override { return m_stream.As<IAppxFileInternal>()->GetCompressedSize(); }
+
+        void Cleanup();
+        
+        enum class State : size_t
+        {
+            UNINITIALIZED = 0,
+            READY_TO_READ,
+            READY_TO_INFLATE,
+            READY_TO_COPY,
+            CLEANUP,
+            MAX = CLEANUP + 1
+        };
+
+        State m_previous = State::UNINITIALIZED;
+        State m_state    = State::UNINITIALIZED;
+
+        ComPtr<IStream> m_stream;
+        ULONGLONG       m_seekPosition = 0;
+        ULONGLONG       m_uncompressedSize = 0;
+        ULONG           m_bytesRead = 0;
+        std::uint8_t*   m_startCurrentBuffer = nullptr;
+        ULONG           m_inflateWindowPosition = 0;
+        ULONGLONG       m_fileCurrentWindowPositionEnd = 0;
+        ULONGLONG       m_fileCurrentPosition = 0;
+
+        z_stream        m_zstrm;
+        int             m_zret;
+
+        std::unique_ptr<std::vector<std::uint8_t>> m_compressedBuffer;
+        std::unique_ptr<std::vector<std::uint8_t>> m_inflateWindow;
+    };
+}
