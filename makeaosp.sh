5818f1dc60c93c0be0a6769d74284bffa1968a89
Ruben Guerrero Samaniego <rubengu@microsoft.com>
2019-03-07 14:09:19 -0800

    ndk problems

--- makeaosp.sh
+++ makeaosp.sh
@@ -76,20 +76,23 @@ while [ "$1" != "" ]; do
     shift
 done
 
-if [ -z "$ndk" ] && [ -n "$ANDROID_NDK_ROOT" ]; then
-    ndk="$ANDROID_NDK_ROOT"
-elif [ -z "$ndk" ] && [ -n "$ANDROID_ROOT"]; then
-    ndk="$ANDROID_ROOT"
-elif [ -z "$ndk" ]; then
-    echo "Android NDK not found"
-    exit 1
-fi
-
 if [ -z "$sdk" ] && [ -n "$ANDROID_HOME" ]; then
     sdk="$ANDROID_HOME"
 elif [ -z "$sdk" ]; then
     echo "Android SDK not found"
     exit 1
+fi
+
+if [ -z "$ndk" ] && [ -n "$ANDROID_NDK_ROOT" ]; then
+    ndk="$ANDROID_NDK_ROOT"
+elif [ -z "$ndk" ] && [ -n "$ANDROID_ROOT"]; then
+    ndk="$ANDROID_ROOT"
+fi
+
+# maybe ANDROID_NDK_ROOT or ANDROID_ROOT is set?
+if [ -z "$ndk" ]; then
+    # if we find the sdk lets just hope they have the ndk
+    ndk="ANDROID_HOME/ndk-bundle"
 fi
 
 printsetup
@@ -98,7 +101,7 @@ mkdir .vs
 cd .vs
 
 # clean up any old builds of msix modules
-find . -depth -name *msix* | xargs -0 -r rm -rf
+find . -name *msix* -d | xargs rm -r
 
 cmake -DCMAKE_SYSTEM_NAME=Android \
     -DCMAKE_ANDROID_NDK="$ndk" \
