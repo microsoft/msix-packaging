a0d97a8ebeae73820e7d0649655178fe0019bad8
Ruben Guerrero <rubengu@microsoft.com>
2019-03-07 18:08:48 -0800

    enabling other tests

--- test/mobile/common/MobileTests.cpp
+++ test/mobile/common/MobileTests.cpp
@@ -123,7 +123,7 @@ static HRESULT RunTestsInternal(std::string source, std::string target)
     hr = RunTest(source + "IntlPackage.appx", unpackFolder, ss, 0);
     hr = RunTest(source + "SignatureNotLastPart-ERROR_BAD_FORMAT.appx", unpackFolder, full, 66);
     hr = RunTest(source + "SignedTamperedBlockMap-TRUST_E_BAD_DIGEST.appx", unpackFolder, full, 66);
-    /*hr = RunTest(source + "SignedTamperedBlockMap-TRUST_E_BAD_DIGEST.appx", unpackFolder, sv, 65);
+    //hr = RunTest(source + "SignedTamperedBlockMap-TRUST_E_BAD_DIGEST.appx", unpackFolder, sv, 65);
     hr = RunTest(source + "SignedTamperedCD-TRUST_E_BAD_DIGEST.appx", unpackFolder, full, 66);
     hr = RunTest(source + "SignedTamperedCodeIntegrity-TRUST_E_BAD_DIGEST.appx", unpackFolder, full, 66);
     hr = RunTest(source + "SignedTamperedContentTypes-TRUST_E_BAD_DIGEST.appx", unpackFolder, full, 66);
@@ -181,7 +181,7 @@ static HRESULT RunTestsInternal(std::string source, std::string target)
     // TODO: add validation that all the files extracted are correct for Android and iOS and add test
     //       that removes a payload package from a flat bundle in the device flat bundle and 
     //       verify that it fails properly.
-*/
+
     std::cout << "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=" << std::endl;
     if(g_TestFailed)
     {   std::cout << "                           FAILED                                 " << std::endl;
