ffaf6b4ca862e8c75dea1ab58263e4fd899661ac
William Cheng <wcheng@ntdev.microsoft.com>
2019-03-08 18:10:11 -0800

    Add protocol implementation

--- preview/Win7Msix/Win7MSIXInstaller/Protocol.cpp
+++ preview/Win7Msix/Win7MSIXInstaller/Protocol.cpp
@@ -6,11 +6,58 @@
 #include "FilePaths.hpp"
 #include "Protocol.hpp"
 #include "GeneralUtil.hpp"
+#include "Constants.hpp"
 #include <TraceLoggingProvider.h>
 
 const PCWSTR Protocol::HandlerName = L"Protocol";
 
-HRESULT Protocol::ExecuteForAddRequest()
+HRESULT Protocol::ParseProtocolElement(IMsixElement* protocolElement)
+{
+    Text<wchar_t> protocolName;
+    RETURN_IF_FAILED(protocolElement->GetAttributeValue(nameAttribute.c_str(), &protocolName));
+
+    ProtocolData protocol;
+    protocol.name = protocolName.Get();
+
+    TraceLoggingWrite(g_MsixTraceLoggingProvider,
+        "Parsing Protocol",
+        TraceLoggingValue(protocolName.Get(), "Name"));
+
+    BOOL hasCurrent = FALSE;
+    ComPtr<IMsixElementEnumerator> logoEnum;
+    RETURN_IF_FAILED(protocolElement->GetElements(logoQuery.c_str(), &logoEnum));
+    RETURN_IF_FAILED(logoEnum->GetHasCurrent(&hasCurrent));
+    if (hasCurrent)
+    {
+        ComPtr<IMsixElement> logoElement;
+        RETURN_IF_FAILED(logoEnum->GetCurrent(&logoElement));
+
+        Text<wchar_t> logoPath;
+        RETURN_IF_FAILED(logoElement->GetText(&logoPath));
+
+        protocol.logo = m_msixRequest->GetPackageInfo()->GetPackageDirectoryPath() + std::wstring(L"\\") + logoPath.Get();
+    }
+
+    ComPtr<IMsixElementEnumerator> displayNameEnum;
+    RETURN_IF_FAILED(protocolElement->GetElements(displayNameQuery.c_str(), &displayNameEnum));
+    RETURN_IF_FAILED(displayNameEnum->GetHasCurrent(&hasCurrent));
+    if (hasCurrent)
+    {
+        ComPtr<IMsixElement> displayNameElement;
+        RETURN_IF_FAILED(displayNameEnum->GetCurrent(&displayNameElement));
+
+        Text<wchar_t> displayName;
+        RETURN_IF_FAILED(displayNameElement->GetText(&displayName));
+
+        protocol.displayName = displayName.Get();
+    }
+
+    m_protocols.push_back(protocol);
+
+    return S_OK;
+}
+
+HRESULT Protocol::ParseManifest()
 {
     ComPtr<IMsixDocumentElement> domElement;
     RETURN_IF_FAILED(m_msixRequest->GetPackageInfo()->GetManifestReader()->QueryInterface(UuidOfImpl<IMsixDocumentElement>::iid, reinterpret_cast<void**>(&domElement)));
@@ -19,7 +66,7 @@ HRESULT Protocol::ExecuteForAddRequest()
     RETURN_IF_FAILED(domElement->GetDocumentElement(&element));
 
     ComPtr<IMsixElementEnumerator> extensionEnum;
-    RETURN_IF_FAILED(element->GetElements(L"/*[local-name()='Package']/*[local-name()='Applications']/*[local-name()='Application']/*[local-name()='Extensions']/*[local-name()='Extension']", &extensionEnum));
+    RETURN_IF_FAILED(element->GetElements(extensionQuery.c_str(), &extensionEnum));
     BOOL hasCurrent = FALSE;
     RETURN_IF_FAILED(extensionEnum->GetHasCurrent(&hasCurrent));
     while (hasCurrent)
@@ -27,13 +74,13 @@ HRESULT Protocol::ExecuteForAddRequest()
         ComPtr<IMsixElement> extensionElement;
         RETURN_IF_FAILED(extensionEnum->GetCurrent(&extensionElement));
         Text<wchar_t> extensionCategory;
-        RETURN_IF_FAILED(extensionElement->GetAttributeValue(L"Category", &extensionCategory));
+        RETURN_IF_FAILED(extensionElement->GetAttributeValue(categoryAttribute.c_str(), &extensionCategory));
 
-        if (wcscmp(extensionCategory.Get(), L"windows.protocol") == 0)
+        if (wcscmp(extensionCategory.Get(), protocolCategoryNameInManifest.c_str()) == 0)
         {
             BOOL hc_protocol;
             ComPtr<IMsixElementEnumerator> protocolEnum;
-            RETURN_IF_FAILED(extensionElement->GetElements(L"*[local-name()='Protocol']", &protocolEnum));
+            RETURN_IF_FAILED(extensionElement->GetElements(protocolQuery.c_str(), &protocolEnum));
             RETURN_IF_FAILED(protocolEnum->GetHasCurrent(&hc_protocol));
 
             if (hc_protocol)
@@ -41,13 +88,51 @@ HRESULT Protocol::ExecuteForAddRequest()
                 ComPtr<IMsixElement> protocolElement;
                 RETURN_IF_FAILED(protocolEnum->GetCurrent(&protocolElement));
 
-                Text<wchar_t> name;
-                RETURN_IF_FAILED(protocolElement->GetAttributeValue(L"Name", &name));
-                
+                RETURN_IF_FAILED(ParseProtocolElement(protocolElement.Get()));
             }
         }
         RETURN_IF_FAILED(extensionEnum->MoveNext(&hasCurrent));
     }
+
+    return S_OK;
+}
+
+HRESULT Protocol::ExecuteForAddRequest()
+{
+    for (std::vector<ProtocolData>::iterator protocol = m_protocols.begin(); protocol != m_protocols.end(); ++protocol)
+    {
+        RETURN_IF_FAILED(ProcessProtocolForAdd(*protocol));
+    }
+
+    return S_OK;
+}
+
+HRESULT Protocol::ProcessProtocolForAdd(ProtocolData& protocol)
+{
+    RegistryKey protocolKey;
+    RETURN_IF_FAILED(m_classesKey.CreateSubKey(protocol.name.c_str(), KEY_WRITE, &protocolKey));
+
+    RETURN_IF_FAILED(protocolKey.SetValue(protocolValueName.c_str(), nullptr, 0, REG_SZ));
+
+    if (protocol.logo.c_str() != nullptr)
+    {
+        RegistryKey defaultIconKey;
+        RETURN_IF_FAILED(protocolKey.CreateSubKey(defaultIconKeyName.c_str(), KEY_WRITE, &defaultIconKey));
+        RETURN_IF_FAILED(defaultIconKey.SetStringValue(L"", protocol.logo.c_str()));
+    }
+
+    RegistryKey shellKey;
+    RETURN_IF_FAILED(protocolKey.CreateSubKey(shellKeyName.c_str(), KEY_WRITE, &shellKey));
+    RETURN_IF_FAILED(shellKey.SetStringValue(L"", openKeyName));
+
+    RegistryKey openKey;
+    RETURN_IF_FAILED(shellKey.CreateSubKey(openKeyName.c_str(), KEY_WRITE, &openKey));
+
+    RegistryKey commandKey;
+    RETURN_IF_FAILED(openKey.CreateSubKey(commandKeyName.c_str(), KEY_WRITE, &commandKey));
+
+    std::wstring command = m_msixRequest->GetPackageInfo()->GetExecutableFilePath() + commandArgument;
+    RETURN_IF_FAILED(commandKey.SetStringValue(L"", command));
 
     return S_OK;
 }
@@ -59,6 +144,10 @@ HRESULT Protocol::CreateHandler(MsixRequest * msixRequest, IPackageHandler ** in
     {
         return E_OUTOFMEMORY;
     }
+
+    RETURN_IF_FAILED(localInstance->m_classesKey.Open(HKEY_CLASSES_ROOT, nullptr, KEY_READ | KEY_WRITE | WRITE_DAC));
+    RETURN_IF_FAILED(localInstance->ParseManifest());
+
     *instance = localInstance.release();
 
     return S_OK;
--- preview/Win7Msix/Win7MSIXInstaller/Protocol.hpp
+++ preview/Win7Msix/Win7MSIXInstaller/Protocol.hpp
@@ -2,10 +2,21 @@
 
 #include "GeneralUtil.hpp"
 #include "IPackageHandler.hpp"
+#include "RegistryKey.hpp"
+#include <vector>
+
+/// Data structs to be filled in from the information in the manifest
+struct ProtocolData
+{
+    std::wstring name;
+    std::wstring displayName;
+    std::wstring logo;
+};
 
 class Protocol : IPackageHandler
 {
 public:
+    
     HRESULT ExecuteForAddRequest();
 
     static const PCWSTR HandlerName;
@@ -13,9 +24,16 @@ public:
     ~Protocol() {}
 private:
     MsixRequest* m_msixRequest = nullptr;
+    RegistryKey m_classesKey;
+    std::vector<ProtocolData> m_protocols;
 
     Protocol() {}
     Protocol(_In_ MsixRequest* msixRequest) : m_msixRequest(msixRequest) {}
 
+    HRESULT ParseProtocolElement(IMsixElement * protocolElement);
+
+    HRESULT ParseManifest();
+
+    HRESULT ProcessProtocolForAdd(ProtocolData & protocol);
 };
     
