3dd04cc5e43e9646ca14cd05facac719bad362cc
Jyothi Venugopal <jyvenugo@microsoft.com>
2019-03-11 12:46:46 -0700

    Installed apps display only publisher name in ARP

--- preview/Win7Msix/Win7MSIXInstaller/AddRemovePrograms.cpp
+++ preview/Win7Msix/Win7MSIXInstaller/AddRemovePrograms.cpp
@@ -39,7 +39,9 @@ HRESULT AddRemovePrograms::ExecuteForAddRequest()
     RETURN_IF_FAILED(packageKey.SetStringValue(L"UninstallString", uninstallCommand));
 
     std::wstring publisherString(packageInfo->GetPublisher());
-    RETURN_IF_FAILED(packageKey.SetStringValue(L"Publisher", publisherString));
+    auto publisherCommonName = publisherString.substr(publisherString.find_first_of(L"=") + 1,
+        publisherString.find_first_of(L",") - publisherString.find_first_of(L"=") - 1);
+    RETURN_IF_FAILED(packageKey.SetStringValue(L"Publisher", publisherCommonName));
     
     std::wstring versionString(ConvertVersionToString(packageInfo->GetVersion()));
     RETURN_IF_FAILED(packageKey.SetStringValue(L"DisplayVersion", versionString));
