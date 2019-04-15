030533791eb64d42225fee41362e0bc058dc477e
Ruben Guerrero Samaniego <rubengu@microsoft.com>
2019-04-15 14:49:31 -0700

    msix

--- src/msix/msix.cpp
+++ src/msix/msix.cpp
@@ -2,6 +2,11 @@
 //  Copyright (C) 2019 Microsoft.  All rights reserved.
 //  See LICENSE file in the project root for full license information.
 // 
+#include <string>
+#include <memory>
+#include <cstdlib>
+#include <functional>
+
 #include "Exceptions.hpp"
 #include "FileStream.hpp"
 #include "ComHelper.hpp"
@@ -9,10 +14,9 @@
 #include "AppxFactory.hpp"
 #include "Log.hpp"
 
-#include <string>
-#include <memory>
-#include <cstdlib>
-#include <functional>
+// Unpack
+#include "DirectoryObject.hpp"
+#include "AppxPackageObject.hpp"
 
 #ifndef WIN32
 // on non-win32 platforms, compile with -fvisibility=hidden
@@ -119,3 +123,95 @@ MSIX_API HRESULT STDMETHODCALLTYPE CoCreateAppxBundleFactory(
         return static_cast<HRESULT>(MSIX::Error::NotSupported);
     #endif
 }
+
+MSIX_API HRESULT STDMETHODCALLTYPE UnpackPackage(
+    MSIX_PACKUNPACK_OPTION packUnpackOptions,
+    MSIX_VALIDATION_OPTION validationOption,
+    char* utf8SourcePackage,
+    char* utf8Destination) noexcept try
+{
+    ThrowErrorIfNot(MSIX::Error::InvalidParameter, 
+        (utf8SourcePackage != nullptr && utf8Destination != nullptr), 
+        "Invalid parameters"
+    );
+
+    MSIX::ComPtr<IStream> stream;
+    ThrowHrIfFailed(CreateStreamOnFile(utf8SourcePackage, true, &stream));
+    ThrowHrIfFailed(UnpackPackageFromStream(packUnpackOptions, validationOption, stream.Get(), utf8Destination));
+    return static_cast<HRESULT>(MSIX::Error::OK);
+} CATCH_RETURN();
+
+MSIX_API HRESULT STDMETHODCALLTYPE UnpackPackageFromStream(
+    MSIX_PACKUNPACK_OPTION packUnpackOptions,
+    MSIX_VALIDATION_OPTION validationOption,
+    IStream* stream,
+    char* utf8Destination) noexcept try
+{
+    ThrowErrorIfNot(MSIX::Error::InvalidParameter, 
+        (stream != nullptr && utf8Destination != nullptr), 
+        "Invalid parameters"
+    );
+
+    MSIX::ComPtr<IAppxFactory> factory;
+    // We don't need to use the caller's heap here because we're not marshalling any strings
+    // out to the caller.  So default to new / delete[] and be done with it!
+    ThrowHrIfFailed(CoCreateAppxFactoryWithHeap(InternalAllocate, InternalFree, validationOption, &factory));
+
+    MSIX::ComPtr<IAppxPackageReader> reader;
+    ThrowHrIfFailed(factory->CreatePackageReader(stream, &reader));
+
+    auto to = MSIX::ComPtr<IStorageObject>::Make<MSIX::DirectoryObject>(utf8Destination);
+    reader.As<IPackage>()->Unpack(packUnpackOptions, to.Get());
+    return static_cast<HRESULT>(MSIX::Error::OK);
+} CATCH_RETURN();
+
+MSIX_API HRESULT STDMETHODCALLTYPE UnpackBundle(
+    MSIX_PACKUNPACK_OPTION packUnpackOptions,
+    MSIX_VALIDATION_OPTION validationOption,
+    MSIX_APPLICABILITY_OPTIONS applicabilityOptions,
+    char* utf8SourcePackage,
+    char* utf8Destination) noexcept try
+{
+#ifdef BUNDLE_SUPPORT
+    ThrowErrorIfNot(MSIX::Error::InvalidParameter, 
+        (utf8SourcePackage != nullptr && utf8Destination != nullptr), 
+        "Invalid parameters"
+    );
+
+    MSIX::ComPtr<IStream> stream;
+    ThrowHrIfFailed(CreateStreamOnFile(utf8SourcePackage, true, &stream));
+    ThrowHrIfFailed(UnpackBundleFromStream(packUnpackOptions, validationOption, applicabilityOptions, stream.Get(), utf8Destination));
+    return static_cast<HRESULT>(MSIX::Error::OK);
+#else
+    return static_cast<HRESULT>(MSIX::Error::NotSupported);
+#endif
+} CATCH_RETURN();
+
+MSIX_API HRESULT STDMETHODCALLTYPE UnpackBundleFromStream(
+    MSIX_PACKUNPACK_OPTION packUnpackOptions,
+    MSIX_VALIDATION_OPTION validationOption,
+    MSIX_APPLICABILITY_OPTIONS applicabilityOptions,
+    IStream* stream,
+    char* utf8Destination) noexcept try
+{
+#ifdef BUNDLE_SUPPORT
+    ThrowErrorIfNot(MSIX::Error::InvalidParameter, 
+        (stream != nullptr && utf8Destination != nullptr), 
+        "Invalid parameters"
+    );
+
+    MSIX::ComPtr<IAppxBundleFactory> factory;
+    // We don't need to use the caller's heap here because we're not marshalling any strings
+    // out to the caller.  So default to new / delete[] and be done with it!
+    ThrowHrIfFailed(CoCreateAppxBundleFactoryWithHeap(InternalAllocate, InternalFree, validationOption, applicabilityOptions, &factory));
+
+    MSIX::ComPtr<IAppxBundleReader> reader;
+    ThrowHrIfFailed(factory->CreateBundleReader(stream, &reader));
+
+    auto to = MSIX::ComPtr<IStorageObject>::Make<MSIX::DirectoryObject>(utf8Destination);
+    reader.As<IPackage>()->Unpack(packUnpackOptions, to.Get());
+    return static_cast<HRESULT>(MSIX::Error::OK);
+#else
+    return static_cast<HRESULT>(MSIX::Error::NotSupported);
+#endif
+} CATCH_RETURN();
