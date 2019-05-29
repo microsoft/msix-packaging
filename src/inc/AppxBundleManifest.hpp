97023eb388cf222c81934638879b1000bef75bb5
Stephen Kwok <stkwok@microsoft.com>
2019-05-29 12:54:43 -0700

    Removed comments

--- src/inc/AppxBundleManifest.hpp
+++ src/inc/AppxBundleManifest.hpp
@@ -26,18 +26,12 @@ public:
 };
 MSIX_INTERFACE(IBundleInfo, 0xff82ffcd,0x747a,0x4df9,0x88,0x79,0x85,0x3a,0xb9,0xdd,0x15,0xa1);
 
-//[
-//	uuid(32e6fcf0-729b-401d-9dbc-f927b494f9af)
-//]
- //{32e6fcf0-729b-401d-9dbc-f927b494f9af}
 #ifndef WIN32
-//interface __declspec(uuid("32e6fcf0-729b-401d-9dbc-f927b494f9af")) IAppxBundleManifestPackageInfoInternal : public IUnknown
 interface IAppxBundleManifestPackageInfoInternal : public IUnknown
 #else
 #include "Unknwn.h"
 #include "Objidl.h"
 class IAppxBundleManifestPackageInfoInternal : public IUnknown
-//class __declspec(uuid("32e6fcf0-729b-401d-9dbc-f927b494f9af")) IAppxBundleManifestPackageInfoInternal : public IUnknown
 #endif
 {
 public:
