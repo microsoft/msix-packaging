35079fd55fbdadc5c4d1d127dbdcff7476caa1b6
John McPherson <johnmcp@microsoft.com>
2019-08-21 12:35:47 -0700

    Forgot to save the last change

--- src/inc/internal/MsixFeatureSelector.hpp
+++ src/inc/internal/MsixFeatureSelector.hpp
@@ -18,6 +18,6 @@
 #ifdef MSIX_PACK
 #define THROW_IF_PACK_NOT_ENABLED
 #else
-#define THROW_IF_PACK_NOT_ENABLED { MSIX::RaiseException<MSIX::Exception>(__LINE__, __FILE__, "Packaging support not enabled", MSIX::Error::SupportExcludedByBuild); }
+#define THROW_IF_PACK_NOT_ENABLED { MSIX::RaiseException<MSIX::Exception>(__LINE__, __FILE__, "Packaging support not enabled", MSIX::Error::SupportExcludedByBuild ); }
 #endif // MSIX_PACK
 #endif // THROW_IF_PACK_NOT_ENABLED
