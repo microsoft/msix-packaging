62cf3fb857f88016fd05c9dec45bb6faff8c1cdc
wcheng-msft <45372824+wcheng-msft@users.noreply.github.com>
2019-11-08 12:22:45 -0800

    Add wix localization (#255)
    * add icon to ARP
    
    * post build works with french
    
    * add postbuild script to generate and embed transforms
    
    * add comments to script and combine for loops
    
    * add localization with placeholder strings

---
+++ preview/MsixCore/MsixMgrWix/postbuild.ps1
@@ -0,0 +1,36 @@
+# Postbuild script to create a multilanguage msi with embedded transforms
+# This creates a separate MSI with UI resources for each language by re-running light.exe with different -cultures: flags
+# Then creates a language-specific transform for each language using WiX tool's torch.exe
+# Then embeds each transform into the MSI using WiSubStg.vbs which was copied from MSI SDK scripts
+
+param (
+    [string]$ProjectDir,
+    [string]$WixExtDir,
+    [string]$TargetDir,
+    [string]$TargetName,
+    [string]$ProjectPath
+ )
+
+$languages = @{ "de-DE" = 1031; "es-ES" = 1034; "fr-FR" = 1036; "it-IT" = 1040; "ja-JP" = 1041; "ko-KR" = 1042; "pt-PT" = 2070; "ru-RU" = 1049; "zh-CN" = 2052; "zh-TW" = 1028 }
+
+$lightExe = "$wixextdir"+ "Light.exe"
+$torchExe = "$wixextdir"+ "Torch.exe"
+$transformEmbedScript = "$ProjectDir\WiSubStg.vbs"
+$wixUIExtensionDll = $WixExtDir + "WixUIExtension.dll"
+
+# keep original copy to create transforms off of, and have a combined copy that has all the embedded transforms
+copy "$TargetDir\en-us\$TargetName.msi" "$TargetDir\$TargetName.msi"
+
+cd $ProjectDir
+$languages.GetEnumerator() |% {
+	$language = $_.key
+	$lcid = $_.value
+
+    #generate an msi for each language (this is already handled by the build)
+	#& "$lightExe" -out $TargetDir\$TargetName-$language.msi -cultures:$language -ext "$wixUIExtensionDll" -sval -wixprojectfile $ProjectPath obj\Release\Product.wixobj
+
+    #create language-specific transform - wrong codepage will generate on error, but the package seems to work anyway.
+	& "$torchExe" $TargetDir\en-us\$TargetName.msi $TargetDir\$language\$TargetName.msi -o $TargetDir\$language.mst | out-null
+    #embed transform into the combined msi
+    & "cscript.exe" $transformEmbedScript $TargetDir\$TargetName.msi $TargetDir\$language.mst $lcid
+}
