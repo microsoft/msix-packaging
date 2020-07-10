af8cbc6dd673c485082a332f17bf63ba4e0ba392
Ruben Guerrero Samaniego <rubengu@microsoft.com>
2020-07-10 11:22:22 -0700

    Better error messages

--- cmake/macos.cmake
+++ cmake/macos.cmake
@@ -1,4 +1,4 @@
-# This is a check to fail arm64 build if the Xcode version is not adecuate.
+# This is a check to fail arm64 build if the Xcode version is not adequate.
 
 # Get xcode version and build.
 execute_process(COMMAND xcodebuild -version OUTPUT_VARIABLE XCODE_OUTPUT ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE)
@@ -14,13 +14,13 @@ if (CMAKE_OSX_ARCHITECTURES MATCHES "arm64")
     if (XCODE_VERSION LESS 12.0)
         # If you see this and you have Xcode-beta 12 do:
         # sudo xcode-select -switch /Applications/Xcode-beta.app/Contents/Developer
-        message(FATAL_ERROR "arm64 is not supported for Xcode version ${XCODE_VERSION}")
+        message(FATAL_ERROR "arm64 is only supported on Xcode 12 12A8161k at this time. Found version ${XCODE_VERSION}. To enable arm64 builds please download Xcode beta 12 build 12A8161k and run `sudo xcode-select -switch /Applications/Xcode-beta.app/Contents/Developer`")
     endif()
 
     # WARNING, this will be false once Xcode 12 is release. Currently there are two
     # Xcode-beta 12, build 12A8161k and build 12A6163b. The latter does NOT support arm64
     if (NOT XCODE_BUILD MATCHES "12A8161k")
-        message(FATAL_ERROR "arm64 is not supported for Xcode build ${XCODE_BUILD}")
+        message(FATAL_ERROR "arm64 is only supported on Xcode 12 12A8161k at this time. Found build ${XCODE_BUILD}")
     endif()
 
 endif()
