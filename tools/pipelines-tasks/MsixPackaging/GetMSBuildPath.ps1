744424acc78bb40b36546722aedd726a08a5370b
Flor Chacón <14323496+florelis@users.noreply.github.com>
2022-12-12 13:30:10 -0800

    Update dependencies for AzDO tasks (#551)

--- tools/pipelines-tasks/MsixPackaging/GetMSBuildPath.ps1
+++ tools/pipelines-tasks/MsixPackaging/GetMSBuildPath.ps1
@@ -4,6 +4,6 @@ param([string]$PreferredVersion, [string]$Architecture)
 # The MSBuild helpers need the VSTS Task SDK.
 # Import it before so it is available.
 Import-Module $PSScriptRoot/ps_modules/VstsTaskSdk
-Import-Module $PSScriptRoot/MSBuildHelpers/MSBuildHelpers
+Import-Module $PSScriptRoot/node_modules/azure-pipelines-tasks-msbuildhelpers/MSBuildHelpers.psm1
 
 return Select-MSBuildPath -PreferredVersion $PreferredVersion -Architecture $Architecture 3>$null 6>$null
