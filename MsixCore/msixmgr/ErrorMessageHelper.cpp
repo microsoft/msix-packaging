c38b11373d29bc78a57503487254807a32fd8e70
pratiksinha07 <89442836+pratiksinha07@users.noreply.github.com>
2023-07-18 17:48:57 +0530

    Added code changes for Improving the Error Messages being outputted and to resolve the Github Bug (#590)
    * Added related files for Telemetry Logging
    
    * Added changes for Telemetry to the msixmgr.cpp file
    
    * Added code changes for ErrorCode and ErrorDesc
    
    * Added changes related to Additional Parameter in Commands
    
    * Added Code Comments for CorrelationId and SourceApplicationId
    
    * Added changes to Code comments
    
    * Changed the Operation name to comply with the actual commands, also removed the check for Operation Type for SourceApplicationId and CorrelationID
    
    * Changed the casing for Local variables and parameters. Added OutputImageType to be captured in Telemetry
    
    * Changed the casing of Local Variables, extracting OperationType from Command Line
    
    * Changed the msixmgrTraceLogging from .h file to a .hpp and added a .cpp file for that. Also, removed unnecessary #defines from msixmgrTelemetry.hpp file
    
    * Changed the ErrorDesc to comply with the existing Error Messages being displayed on the Console
    
    * Removed the Telemetry Logging when VHDSize is 0 as it is not needed anymore
    
    * Changed a telemetry logging statement for unpacking int VHD(X)/Folder
    
    * Removed TraceLoggingOptionGroupValue with dummy value
    
    * [Task - 43152318] - Added Comments and Description in the Code for newly added files
    
    * [Task-43971224] Added code changes for Error Code Improvement
    
    * Added few changes to capture no. of failed files during unpack
    
    * [Task - 43971224] Added change to incorporate a change related to indexes being outputted in Console Output
    
    * Removed the code changes related to Github Bug
    
    * [Task - 43971224] Made the Output more descriptive when the failure occurs for VHD Size paramater
    
    * [Task - 43971224] Added code changes to have separate if-else case for CIM and VHD(X) in VHDSize error message
    
    * [Task - 43971224] Changed the ErrorMessageHelper function to handle exceptions using try-catch
    
    * [Task - 43971224] Fixed the code for Exception Handling for ErrorMessageHelper.cpp
    
    * [Task - 43971224] Added the description for the ErrorMessageHelper.cpp file
    
    * [Task - 44528959] Added code changes to resolve the Github Bug

---
+++ MsixCore/msixmgr/ErrorMessageHelper.cpp
@@ -0,0 +1,46 @@
+#pragma once
+
+#include<windows.h>
+#include <afx.h>
+#include <string>
+
+// Purpose:
+// - This file contains the helper function for extracting the Error Description corresponding to
+//   the HRESULT code being thrown during a particular Workflow execution in MSIXMGR
+
+namespace ErrorMessageHelper
+{
+    std::wstring GetErrorMessageFromHRESULT(
+        HRESULT hresult)
+    {
+        CString strMessage;
+        std::wstring errorMessage;
+        try
+        {
+            // use FormatMessage to get a system-defined error message
+            LPTSTR lpMsgBuf = NULL;
+            DWORD dw = FormatMessage(
+                FORMAT_MESSAGE_ALLOCATE_BUFFER |
+                FORMAT_MESSAGE_FROM_SYSTEM |
+                FORMAT_MESSAGE_IGNORE_INSERTS,
+                NULL,
+                hresult,
+                MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+                (LPTSTR)&lpMsgBuf,
+                0,
+                NULL);
+            if (dw > 0) {
+                strMessage.Append(lpMsgBuf);
+                LocalFree(lpMsgBuf);
+            }
+            errorMessage = strMessage.GetString();
+        }
+        catch (...)
+        {
+            errorMessage = L"Error Description could not be found";
+        }
+
+        return errorMessage;
+    }
+
+}
