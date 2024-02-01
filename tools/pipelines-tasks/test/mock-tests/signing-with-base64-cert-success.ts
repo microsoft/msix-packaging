5f555647a14c14f5a8ed415e0fc8e2fbe766795f
Zinnia Mazumder <zmazumder@microsoft.com>
2024-02-01 16:18:25 +0530

    Changed logic to remove credscan errors

--- tools/pipelines-tasks/test/mock-tests/signing-with-base64-cert-success.ts
+++ tools/pipelines-tasks/test/mock-tests/signing-with-base64-cert-success.ts
@@ -2,46 +2,34 @@ import tmrm = require('azure-pipelines-task-lib/mock-run');
 import fs = require('fs');
 import testHelpers = require('../testhelpers');
 import path = require('path');
-//import helpers = require('common-helpers/helpers');
-//import helpers = require('common/helpers');
-import { ToolRunner } from 'azure-pipelines-task-lib/toolrunner';
-import tl = require('azure-pipelines-task-lib/task');
+const pem = require("pem");
 
-const taskMockRunner = new tmrm.TaskMockRunner(testHelpers.TaskEntryPoints.SigningTask);
-const HELPER_SCRIPT = 'C:/Users/zmazumder/source/repos/msix-packaging/tools/pipelines-tasks/EncodeCert.ps1'
-/**
- * Gets a ToolRunner for running a Powershell script.
- * Script arguments can be added later by the caller.
- * @param scriptPath Script to run.
- */
-export const getPowershellRunner = (scriptPath: string): ToolRunner => {
-    const powershellRunner: ToolRunner = tl.tool('powershell')
-        .arg('-NoLogo')
-        .arg('-NoProfile')
-        .arg('-NonInteractive')
-        .arg(['-ExecutionPolicy', 'Unrestricted']);
+process.env['OPENSSL_BIN'] = path.join(__dirname, "..\\..\\node_modules\\pem\\openssl");
 
-    // Quote the script path to allow for spaces.
-    // Existing quotes need to be escaped.
-    powershellRunner.arg(`& '${scriptPath.replace("'", "''")}'`);
-    return powershellRunner;
-}
-async function GetEncodedCertificate() {
-    const powershellRunner: ToolRunner = getPowershellRunner(HELPER_SCRIPT);
-    powershellRunner.arg(['-filePath', '\'' + testHelpers.testCertificate + '\'']);
-
-    let execResult = await powershellRunner.execSync();
-    const certUtf8 = execResult.stdout.trim();
-    console.log(certUtf8);
-    return certUtf8;
+function convertPfxToBase64() {
+    const pfx = fs.readFileSync(testHelpers.testCertificate);
+    return new Promise((resolve, reject) => {
+        pem.readPkcs12(pfx, { p12Password: "password" }, (err :string, cert : any) => {
+            if (err) {
+                reject(err);
+            } else {
+                const pfxCert = cert.cert as string;
+                const utf8Cert = pfxCert.replace(/\n/g, '')
+                    .replace(/-----BEGIN CERTIFICATE-----/, '')
+                    .replace(/-----END CERTIFICATE-----/, '');
+                resolve(utf8Cert);
+            }
+        });
+    });
 }
 
-const encodedCertificate = GetEncodedCertificate();
-/*const base64String = fs.readFileSync(testHelpers.testEncodedCertificate, 'base64').toString().trim();
-const encodedCertificate = Buffer.from(base64String, 'base64').toString('utf-8');*/
-taskMockRunner.setInput('package', testHelpers.testPackage);
-taskMockRunner.setInput('certificateType', 'base64');
-taskMockRunner.setInput('encodedCertificate', encodedCertificate);
+// Call the convertPfxToBase64 function and use the returned certificate
+convertPfxToBase64().then((encodedCertificate) => {
+    const taskMockRunner = new tmrm.TaskMockRunner(testHelpers.TaskEntryPoints.SigningTask);
+    taskMockRunner.setInput('package', testHelpers.testPackage);
+    taskMockRunner.setInput('certificateType', 'base64');
+    taskMockRunner.setInput('encodedCertificate', encodedCertificate as string);
 
-// Don't mock call to signtool
-taskMockRunner.run(/* noMockTask */ true);
+    // Don't mock call to signtool
+    taskMockRunner.run(/* noMockTask */ true);
+});
