aadf9d2e0e27fae005757c58189ab63a6244b859
Zinnia Mazumder <zmazumder@microsoft.com>
2024-02-02 14:11:41 +0530

    Changes to migrate pipeline to 1ES Pipeline Templates (1ESPT)

--- tools/pipelines-tasks/test/mock-tests/signing-with-base64-cert-success.ts
+++ tools/pipelines-tasks/test/mock-tests/signing-with-base64-cert-success.ts
@@ -1,13 +1,36 @@
 import tmrm = require('azure-pipelines-task-lib/mock-run');
 import fs = require('fs');
 import testHelpers = require('../testhelpers');
+import path = require('path');
+const pem = require("pem");
 
-const taskMockRunner = new tmrm.TaskMockRunner(testHelpers.TaskEntryPoints.SigningTask);
+process.env['OPENSSL_BIN'] = path.join(__dirname, "..\\..\\node_modules\\pem\\openssl");
 
-const encodedCertificate = fs.readFileSync(testHelpers.testEncodedCertificate, { encoding: 'utf-8' }).toString().trim();
-taskMockRunner.setInput('package', testHelpers.testPackage);
-taskMockRunner.setInput('certificateType', 'base64');
-taskMockRunner.setInput('encodedCertificate', encodedCertificate);
+//Function to convert pfx to base64 encoded string
+function convertPfxToBase64() {
+    const pfx = fs.readFileSync(testHelpers.testCertificate);
+    return new Promise((resolve, reject) => {
+        pem.readPkcs12(pfx, { p12Password: "password" }, (err: string, cert: any) => {
+            if (err) {
+                reject(err);
+            } else {
+                const pfxCert = cert.cert as string;
+                const utf8Cert = pfxCert.replace(/\n/g, '')
+                    .replace(/-----BEGIN CERTIFICATE-----/, '')
+                    .replace(/-----END CERTIFICATE-----/, '');
+                resolve(utf8Cert);
+            }
+        });
+    });
+}
 
-// Don't mock call to signtool
-taskMockRunner.run(/* noMockTask */ true);
+//Call the function to convert pfx to base64 and then mock the task
+convertPfxToBase64().then((encodedCertificate) => {
+    const taskMockRunner = new tmrm.TaskMockRunner(testHelpers.TaskEntryPoints.SigningTask);
+    taskMockRunner.setInput('package', testHelpers.testPackage);
+    taskMockRunner.setInput('certificateType', 'base64');
+    taskMockRunner.setInput('encodedCertificate', encodedCertificate as string);
+
+    // Don't mock call to signtool
+    taskMockRunner.run(/* noMockTask */ true);
+});
